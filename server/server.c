3d79c73a58094fb9b519586acd07696b799866b9
Eduardo Viana <eduardocosta_viana@live.com>
2019-05-20 09:25:39 -0600

    Adicionando threads e mutex  para gerenciar conexões de clientes

--- server/server.c
+++ server/server.c
@@ -17,17 +17,51 @@ Git: https://github.com/eduvianna/sistema_arquivos.git
 #include <sys/types.h>
 #include <netinet/in.h>
 
+//Biblioteca para threads
+#include <pthread.h>
+
 #define PORT 3000
-#define MSG_SIZE 4096
+#define MSG_SIZE 5000
+#define BUFFER_SIZE 1000
+#define N_THREAD 10
+
+//Declaração buffer e client message
+char client_message[MSG_SIZE],
+    buffer[BUFFER_SIZE];
+
+void *connect_thread(void *arg)
+{
+    int new_sockfd = *((int *)arg);
+    recv(new_sockfd, client_message, MSG_SIZE, 0);
+
+    //Tratamento de mensagens entre cliente e servidor
+    pthread_mutex_lock(&lock);
+    char *message = malloc(sizeof(client_message) + 20);
+    strcpy(message, "Bem Vindo ao Sistema de Arquivos XingLing!\n\0");
+    strcat(message, client_message);
+    strcpy(buffer, message);
+    free(message);
+    pthread_mutex_unlock(&lock);
+    sleep(1);
+    send(new_sockfd, buffer, 13, 0);
+    printf("Até Mais! \n");
+    close(new_sockfd);
+    pthread_exit(NULL);
+}
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 struct sockaddr_in local;
-struct sockaddr_in remoto;
+struct sockaddr_in remote;
 
 int main()
 {
+
     int sockfd, client;
-    int tam_remoto = sizeof(remoto);
+    int size_remote = sizeof(remote);
     int slen;
-    char buffer[4096];
+
+    //Criando N_THREADS
+    pthread_t client_thread[N_THREAD];
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -54,30 +88,39 @@ int main()
     }
 
     //Coloca a porta para escutar
-    listen(sockfd, 1);
-    client = accept(sockfd, (struct sockaddr *)&remoto, &tam_remoto);
-
-    //Checa se houve erro na conexão do cliente
-    if (client == -1)
-    {
-        perror("Error client");
-        exit(1);
-    }
+    listen(sockfd, N_THREAD);
 
     strcpy(buffer, "Bem Vindo ao Sistema de Arquivos XingLing!\n\0");
 
-    if (send(client, buffer, strlen(buffer), 0))
+    int i = 0;
+    while (1)
     {
-        printf("Aguardando ...\n");
-        while (1)
+        client = accept(sockfd, (struct sockaddr *)&remote, &size_remote);
+
+        //Checa se houve erro na conexão do cliente
+        if (client == -1)
         {
-            memset(buffer, 0x0, MSG_SIZE);
-            if ((slen = recv(client, buffer, MSG_SIZE, 0)) > 0)
+            perror("Error client");
+            exit(1);
+        }
+
+        if (send(client, buffer, strlen(buffer), 0))
+        {
+            pthread_create(&client_thread[i], NULL, connect_thread);
+            printf("Aguardando ...\n");
+            while (1)
             {
-                buffer[slen - 1] = '\0';
-                printf("Recebido: %s\n", buffer);
-                close(client);
-                break;
+                memset(buffer, 0x0, MSG_SIZE);
+                if ((slen = recv(client, buffer, MSG_SIZE, 0)) > 0)
+                {
+                    buffer[slen - 1] = '\0';
+                    printf("Recebido: %s\n", buffer);
+                    if (strcmp(buffer, "exit") == 0)
+                    {
+                        close(client);
+                        break;
+                    }
+                }
             }
         }
     }
